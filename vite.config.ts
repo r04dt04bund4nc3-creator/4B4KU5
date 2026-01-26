66e5e35f3c188e52b03b69b65df367d09ae4394b
r04dt04bund4nc3-creator <r04dt04bund4nc3@gmail.com>
2026-01-26 11:33:48 +0700

    2nd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Explicitly set to root for Vercel. Do not change this.
 })
